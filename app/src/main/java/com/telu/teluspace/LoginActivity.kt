493a43c0dc1611b2da463e0e330f4fe28176159e
krafime <krafime@student.telkomuniversity.ac.id>
2023-05-22 18:07:23 +0700

    tambah activity_login.xml

---
+++ app/src/main/java/com/telu/teluspace/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.telu.teluspace
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.telu.teluspace.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
